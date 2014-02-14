73d107c91efb632cb0b639133dd1a09c51b4e914
bradfordbradford <bradford@wearetelegraph.com>
2014-02-13 22:03:56 -0600

    Feature Article Interactive Elements

--- output/assets/js/app.js
+++ output/assets/js/app.js
@@ -1,3 +1,106 @@
+
+
+
+
+ // Now, Now  //
+jQuery(document).ready(function($) {
+
+    // Slideshow
+    $('.slideshow').flexslider({
+        animation: "slide",
+        controlNav: false,
+        prevText: '',
+        nextText: ''
+    });
+
+
+    // Overlay: View Image
+    $('.overlay-view-image').each(function() { // the containers for all your galleries
+        $(this).magnificPopup({
+              type: 'image',
+              mainClass: 'mfp-zoom-in overlay-with-image',
+              tLoading: '',
+              removalDelay: 100,
+              callbacks: {
+
+                imageLoadComplete: function() {
+                  var self = this;
+                  setTimeout(function() {
+                    self.wrap.addClass('mfp-image-loaded');
+                  }, 16);
+                },
+                close: function() {
+                  this.wrap.removeClass('mfp-image-loaded');
+                },
+
+                // avoid caching of image
+                beforeChange: function() {
+                 this.items[0].src = this.items[0].src + '?=' + Math.random();
+                }
+              },
+
+              closeBtnInside: false,
+              closeOnContentClick: true,
+              midClick: true
+        });
+    });
+
+    // Overlay: Watch Video
+    $('.overlay-watch-video').each(function() { // the containers for all your galleries
+        $(this).magnificPopup({
+            type: 'iframe',
+            mainClass: 'mfp-zoom-in overlay-with-video',
+            removalDelay: 100,
+            preloader: false,
+            fixedContentPos: false,
+            closeBtnInside: false,
+            closeOnContentClick: true,
+            midClick: true
+        });
+    });
+
+    // Overlay: View Slideshow
+    $('.overlay-view-slideshow').magnificPopup({
+          delegate: 'a',
+          type: 'image',
+          tLoading: '',
+          mainClass: 'mfp-zoom-in overlay-with-slideshow-slides',
+          removalDelay: 100,
+          callbacks: {
+            imageLoadComplete: function() {
+              var self = this;
+              setTimeout(function() {
+                self.wrap.addClass('mfp-image-loaded');
+              }, 16);
+            },
+            close: function() {
+              this.wrap.removeClass('mfp-image-loaded');
+            },
+
+          },
+          closeBtnInside: false,
+          closeOnContentClick: true,
+          midClick: true,
+          gallery: {
+            enabled: true,
+            navigateByImgClick: true,
+            preload: [0,1] // Will preload 0 - before current, and 1 after the current image
+          },
+          image: {
+            tError: '<a href="%url%">The image #%curr%</a> could not be loaded.',
+            titleSrc: function(item) {
+              return item.el.attr('title') + '<small>by Marsel Van Oosten</small>';
+            }
+          }
+        });
+
+    //  ----------
+    // $(function() {
+    // });
+
+});
+
+
 function adaptMagic(i, width) {
 document.documentElement.id = 'range_' + i;
 }
@@ -15,18 +118,3 @@ range: [
 '1824'
 ]
 };
-
-
-
-//  Now, Now  //
-jQuery(document).ready(function($) {
-
-    //  ----------
-    $(function() {
-
-    });
-
-});
-
-
-
