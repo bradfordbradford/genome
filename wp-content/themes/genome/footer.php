fbe54a9a7f87e2dfa2af68b7df31467c601d0db1
bradfordbradford <bradford@wearetelegraph.com>
2014-03-31 13:38:12 -0500

    Added Genome AddThis ID

--- wp-content/themes/genome/footer.php
+++ wp-content/themes/genome/footer.php
@@ -101,7 +101,7 @@
     <!-- / window.jQuery || document.write('<script src="../js/libs/jquery.min.js"><\/script>') -->
     <script src='<?php echo get_stylesheet_directory_uri(); ?>/js/min/app.min.js'></script>
     <script src='<?php echo get_stylesheet_directory_uri(); ?>/js/min/sitescripts.min.js'></script>
-    <script src='//s7.addthis.com/js/300/addthis_widget.js#pubid=ra-509d72c21cd49086'></script>
+    <script src='//s7.addthis.com/js/300/addthis_widget.js#pubid=ra-5339b4d16c709f80'></script>
     <script>
       var addthis_config = {"data_track_addressbar":true};
     </script>
